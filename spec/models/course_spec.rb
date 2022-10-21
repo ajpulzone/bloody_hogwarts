51d44ac4c9fb8fc0eb544f5432139f4dedf3b18c
Annie Pulzone <ajpulzone@gmail.com>
2022-10-20 19:33:13 -0700

    Completed repo through user story 4, practice IC group walkthrough

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -11,4 +11,19 @@ RSpec.describe Course, type: :model do
     it {should have_many(:students).through(:student_courses)}
   end
 
+  describe "behavior" do
+    it "#student_count" do
+      student = Student.create!(name: "Casseopia Black", age: 14, house: "Slytherin")
+      student2 = Student.create!(name: "Sirius Black", age: 16, house: "Gryffindor")
+      student3 = Student.create!(name: "Regulus", age: 15, house: "Slytherin")
+      course1 = Course.create!(name: "Defense against the Dark Arts")
+      course2 = Course.create!(name: "Charms")
+      student.courses << course1
+      student2.courses << course2
+      student3.courses << course1
+
+      expect(course1.student_count).to eq(2)
+      expect(course2.student_count).to eq(1)     
+    end
+  end
 end
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -13,4 +13,15 @@ RSpec.describe Student, type: :model do
     it {should have_many(:courses).through(:student_courses)}
   end
 
+  describe "class methods" do
+    describe "#average_age" do
+      it "shows the average age of all students" do
+        student = Student.create!(name: "Casseopia Black", age: 14, house: "Slytherin")
+        student1 = Student.create!(name: "Harry", age: 19, house: "Slytherin")
+        student2 = Student.create!(name: "Ron", age: 18, house: "Slytherin")
+      
+        expect(Student.average_age).to eq(17.0)
+      end
+    end
+  end
 end
